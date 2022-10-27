175643a1958c5ce275f3679a5cf671f4a8bd9bea
moriss <marrocco001@gmail.com>
2022-10-27 11:13:30 +0100

    Update terms.c

--- terms.c
+++ terms.c
@@ -1,7 +1,7 @@
 <?php
 
-//This term is: Passing values to a function
-//The 30,30 in the "add" parameters below are the values you are passing in 
+//This term is: Passing arguments to a function
+//The 30,30 in the "add" parameters below are the arguments (or values) you are passing into the function.
 function add($num1 , $num2){
 return $num1 * $num2;
 }
