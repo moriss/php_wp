e6a4512e2b5549dd0dc6df16592793720f30829e
moriss <marrocco001@gmail.com>
2022-10-27 11:12:17 +0100

    Create terms.c

---
+++ terms.c
@@ -0,0 +1,9 @@
+<?php
+
+//This term is: Passing values to a function
+//The 30,30 in the "add" parameters below are the values you are passing in 
+function add($num1 , $num2){
+return $num1 * $num2;
+}
+echo "<h2> The answer to 30 times 30 is " . add(30,30 ) . "<h2>";
+?>
