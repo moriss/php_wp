375ef078d109d6743254d0fb83b9b2dc1f522556
moriss <marrocco001@gmail.com>
2022-10-28 19:44:00 +0100

    Update 3.include.php

--- 3.include.php
+++ 3.include.php
@@ -1,5 +1,5 @@
 #A brief explanation of "include" in php:
-To avoid repetitive code, you can create seperate header and footer files that can be included in you chosen additional web pages. 
+To avoid repetitive code, you can create seperate header and footer files that can be included in your chosen additional web pages. 
 See below:
 (NB: replace "include" with "require" if you do NOT want other parts of the web page showing if the file you wish to include is missing).
 
