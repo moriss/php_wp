d27b602b86d1111a03ccb73832b04cb234c72da6
moriss <marrocco001@gmail.com>
2023-03-01 15:40:59 +0000

    Update terms.c

--- terms.c
+++ terms.c
@@ -7,3 +7,10 @@ return $num1 * $num2;
 }
 echo "<h2> The answer to 30 times 30 is " . add(30,30 ) . "<h2>";
 ?>
+  
+
+init: // Initialization code (or boot code) takes the processor from the reset state to a state where the operating system can run
+
+add_action
+//The add_action function is arguably the most used function in WordPress. Simply put, it allows you to run a function when a particular hook occurs
+
