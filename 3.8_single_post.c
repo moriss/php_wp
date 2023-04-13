b73ad4a1b25a1d5f529bc16fa74fe45af2011e4c
moriss <marrocco001@gmail.com>
2023-04-13 10:10:11 +0100

    Create 3.8_single_post.c

---
+++ 3.8_single_post.c
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<h1>single.php</h1>
+
+<?php
+	while(have_posts()) {  //NB have_posts require comments
+		
+		the_post();
+?>
+		<h2> <?php the_title();?> </h2>
+		
+		Posted by <?php the_author(); ?>
+		
+		<img src= "<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" />
+		
+		<?php the_content(); 
+		      'the_comment_form()'; 
+
+		 } 	?>
+
+<?php get_footer(); ?>
