5b754f0b17b0b02e4452ec47b64b5ea9ea16eb81
moriss <marrocco001@gmail.com>
2023-04-13 15:55:18 +0100

    Update 3.7post_list.c

--- 3.7post_list.c
+++ 3.7post_list.c
@@ -1,16 +1,8 @@
 //The below code sits between the header and footer hooks on the index page.
 
 <?php  
-
-$args = array ( //an associative array
-	'post_type' => 'project',
-	'posts_per_page' => 3
-);
-
-	$blogposts = new WP_Query($args);
-
-	while($blogposts -> have_posts()) {  //The condition of the while loop is that posts exist.
-		$blogposts -> the_post(); //For now, I'm labelling this a 'gateway' -as such- into out-putting below.
+	while(have_posts()) {  //The condition of the while loop is that posts exist.
+			the_post(); //For now, I'm labelling this a 'gateway' -as such- into out-putting below.
    //You need to close the php environment to allow the html h3 tags to work. ?>
    
    <a href="<?php the_permalink(); ?>" >
