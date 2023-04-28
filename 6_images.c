aa0904921f3f09dd16e02ee24fb7132a339d4b46
moriss <marrocco001@gmail.com>
2023-04-28 08:52:53 +0100

    Update 6_images.c

--- 6_images.c
+++ 6_images.c
@@ -1,2 +1,3 @@
 //Adding a logo to the header.php file
 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="grange">
+//The first part   get_template_directory_uri();   goes to the route folder of your wordpress site. 
