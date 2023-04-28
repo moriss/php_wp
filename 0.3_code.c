9ea50e30ecf293d87c07882a71359ff2d34d939d
moriss <marrocco001@gmail.com>
2023-04-28 10:19:55 +0100

    Create 0.3_code.c

---
+++ 0.3_code.c
@@ -0,0 +1,20 @@
+//Finds the route of your wp site
+<?php echo get_template_directory_uri(); ?>
+//So this would be how you display an image from your img folder
+  <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt = "Grange Logo"/>
+  
+  // Resides in the header.php file. This grabs link code from the functions.php file relating to linking your css and javascript files to your wp site. 
+  <?php wp_head();  /****/ ?>
+  // It replaces all the link script from a typical static page for your css, js, font awsome and google fonts etc.
+  //In addition, I think it also identifies the file as the header.php file
+  
+  // Resides at the bottom of the footer.php file and identifies it as such.
+  <?php wp_footer(); ?>
+  
+  //Resides on the 'main' page you wish to header.php and footer.php code to be displayed.
+  // Examples of 'main' page are index.php or front-page or subsequent pages of your choosing.
+  //This embeds the header.php file into your wordpress page.
+  <?php get_header(); ?>
+  // This line of code goes wherever you want the header.php code to go
+  // And below does exactly the same for the footer.php file
+  <?php get_footer(); ?>
