ea07d3247911f75a57591f0598b1ee7dd681a548
moriss <marrocco001@gmail.com>
2023-05-05 10:11:45 +0100

    Update 6_images.c

--- 6_images.c
+++ 6_images.c
@@ -1,3 +1,8 @@
+//This code is more speific to images
+
 //Adding a logo to the header.php file
 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="grange">
 //The first part   get_template_directory_uri();   goes to the route folder of your wordpress site. 
+
+
+<img src="<?php echo get_the_post_thumbnail_url('get_the_ID'); ?>" //Gets your thumbnail image
