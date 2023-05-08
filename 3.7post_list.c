ac2258ccd6015a5cb78cbacbd2783043a70b1cda
moriss <marrocco001@gmail.com>
2023-05-08 15:14:50 +0100

    Update 3.7post_list.c

--- 3.7post_list.c
+++ 3.7post_list.c
@@ -17,24 +17,27 @@
 			}
 		?>
 //a) Post list has: while have posts, the post, the title, the excerpt
+//function.php
 //for this, function.php requires:  add theme support, post thumbnails, title tag, html5(for comment list, comment-form and search form)
-//And  add_action
+//And  add_action 
 
 
 
 
 
-//or
-// $1 post type, posts per page),  $2 WP Query($1),  while h$ ave posts,   $ the post,   permalink,  title,  excerpt
 
 
+
+
+//For more functionality:
+// $1[post type, posts per page],  $2 WP Query($1),  while h$ ave posts,   $ the post,   permalink,  title,  excerpt
 
 //Second block of code
 //This next block allows for wordpress customised queries including choice of posts to be displayed, (post type) , and number of posts per page (posts per page) 
 //
 <?php  
   $args = array( //Create a variable with an associative array that points to the 'project' post type you created
- 	 'post_type'  => 'project' //associative array declaring which post to display
+ 	 'post_type'  => 'post' //associative array declaring which post to display
 	 'posts_per_page' => 2, // How many posts per page should be displayed
  );
  
@@ -53,7 +56,7 @@
 			}
 		?>
 
-			
+
 
 //Third block of code
 //This is a live example of how you added the post list to the grange website's front page. You will notice that the while loop's closing curly brace is just after  'the_post' . Then only after this do you have various custom queries such as: permalink. get_the_post_thumbnail,  the_title(); trim_words.
